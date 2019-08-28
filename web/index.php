64b0e0709fa019707de0e51a4b40d32d828d7e7b
EC2 Default User <ec2-user@ip-172-31-91-190.ec2.internal>
2019-08-28 15:29:32 +0000

    linebot.php

--- web/index.php
+++ web/index.php
@@ -1,33 +1,10 @@
-<?php
-DEFINE("ACCESS_TOKEN","srEsDTuULK9VJ99DbklXd4Rg0aoVtH+U1aqwKcYrjg5kVGXtzJC4xZ/MN6iHqN6qXTTs6goHAeTnIwFv7dcMcWiQ3NgDiP1GRNO7Zd9SW47CwAL7uWfDi0X50SHBEODEHiC0qzM3ki+wmoGAGAAIdgdB04t89/1O/w1cDnyilFU=");
-DEFINE("SECRET_TOKEN","eb6a16a96935ac82207dc092cca715d2");
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
 
-use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use \LINE\LINEBot;
-use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
-use \LINE\LINEBot\Constant\HTTPHeader;
-
-//LINESDKの読み込み
-require_once(__DIR__."/vendor/autoload.php");
-
-//LINEから送られてきたらtrueになる
-if(isset($_SERVER["HTTP_".HTTPHeader::LINE_SIGNATURE])){
-
-//LINEBOTにPOSTで送られてきた生データの取得
-  $inputData = file_get_contents("php://input");
-
-//LINEBOTSDKの設定
-  $httpClient = new CurlHTTPClient(ACCESS_TOKEN);
-  $Bot = new LINEBot($HttpClient, ['channelSecret' => SECRET_TOKEN]);
-  $signature = $_SERVER["HTTP_".HTTPHeader::LINE_SIGNATURE]; 
-  $Events = $Bot->parseEventRequest($InputData, $Signature);
-
-//大量にメッセージが送られると複数分のデータが同時に送られてくるため、foreachをしている。
-  foreach($Events as $event){
-    $SendMessage = new MultiMessageBuilder();
-    $TextMessageBuilder = new TextMessageBuilder("よろぽん！");
-    $SendMessage->add($TextMessageBuilder);
-    $Bot->replyMessage($event->getReplyToken(), $SendMessage);
-  }
-}
+<title>no page</title>
+</head>
+<body>
+ <h1>No Index</h1>
+</body>
+</html>
