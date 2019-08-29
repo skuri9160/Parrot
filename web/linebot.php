1e260d20eff46097e1e1d83d7af89e9c5ab7c0ac
EC2 Default User <ec2-user@ip-172-31-91-190.ec2.internal>
2019-08-29 14:33:14 +0000

    a

--- web/linebot.php
+++ web/linebot.php
@@ -24,17 +24,15 @@ if(isset($_SERVER["HTTP_".HTTPHeader::LINE_SIGNATURE])){
   $signature = $_SERVER["HTTP_".HTTPHeader::LINE_SIGNATURE]; 
   $Events = $Bot->parseEventRequest($inputData, $signature);
 
-//大量にメッセージが送られると複数分のデータが同時に送られてくるため、foreachをしている。
-  foreach($Events as $event){
-    
     $res = $bot->replyMessage($event->getReplyToken(), new TextMessageBuilder('メッセージが来たよ！'));
     if ($res->isSucceeded()) {
       error_log('success!!');
     } else {
       error_log("深刻な返信エラー" . $res->getHTTPStatus() . ' ' . $res->getRawBody());
     }
-  
-  
+
+//大量にメッセージが送られると複数分のデータが同時に送られてくるため、foreachをしている。
+  foreach($Events as $event){
     $SendMessage = new MultiMessageBuilder();
     $TextMessageBuilder = new TextMessageBuilder("よろぽん！");
     $SendMessage->add($TextMessageBuilder);
